44807ca0b43d433608a29a9162f430c9e6dcd0f4
Nicolás Cabrera <jncabdom@gmail.com>
2023-12-28 17:20:19 +0100

    Restructuring project. API recording values into neo4j

--- deploy_services.sh
+++ deploy_services.sh
@@ -3,10 +3,13 @@
 kubectl delete deployments --all
 kubectl delete services --all
 
-docker build -t service:latest .
+docker build -t service:latest ./service-app
+docker build -t tracker:latest ./tracking-api
 
 kubectl apply -f "k8s/services/neo4j-pvc.yaml"
 kubectl apply -f "k8s/deployments/neo4j-deployment.yaml"
+kubectl apply -f "k8s/deployments/tracker-deployment.yaml"
+kubectl apply -f "k8s/services/tracker-service.yaml"
 kubectl apply -f "k8s/services/neo4j-service.yaml"
 
 for file in k8s/deployments/service*-deployment.yaml; do
---
+++ tracking-api/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+from neo4j import GraphDatabase
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+
+# Configure Neo4j connection
+uri = os.getenv('NEO4J_URI', "bolt://192.168.49.2:30687")
+username = os.getenv('NEO4J_USER', "neo4j")
+password = os.getenv('NEO4J_PASSWORD', "password")
+driver = GraphDatabase.driver(uri, auth=(username, password))
+
+def create_ping(tx, origin, destiny, timeStamp):
+    query = """
+    CREATE (p:Ping {origin: $origin, destiny: $destiny, timeStamp: $timeStamp})
+    RETURN p.origin AS origin, p.destiny AS destiny, p.timeStamp AS timeStamp
+    """
+    result = tx.run(query, origin=origin, destiny=destiny, timeStamp=timeStamp)
+    return result.single()
+
+@app.route('/pings', methods=['POST'])
+def record_ping():
+    data = request.json
+    origin = data['origin']
+    destiny = data['destiny']
+    timeStamp = data.get('timeStamp', datetime.utcnow().isoformat())
+
+    try:
+        with driver.session() as session:
+            ping = session.write_transaction(create_ping, origin, destiny, timeStamp)
+            if ping:
+                return jsonify(ping), 201
+            else:
+                return jsonify({"error": "No record created"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/health', methods=['GET'])
+def health():
+    return "API is running", 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
